b1a72a903666a17d617d90581283f7878550c358
Oleg K <brunneng9@gmail.com>
2019-04-25 16:55:09 +0300

    Task4

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -10,6 +10,30 @@ namespace Palindrome
     {
         static void Main(string[] args)
         {
-        }
+            Console.WriteLine("Please enter a word");
+            string word = Console.ReadLine();
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (Char.IsWhiteSpace(word[i]))
+                {
+                    Console.WriteLine("It should be only one word without spaces");
+                    return;
+                }
+            }
+            for (int i = 0; i < word.Length; i++)
+            {
+                if ( word[i] == 32) 
+                {
+                    Console.WriteLine("It should be only one word");
+                    return;
+                }
+                else if ( word[i] != word[(word.Length - (i+1))])
+                {
+                    Console.WriteLine($"The word {word} is NOT a palindrome");
+                    return;
+                }
+            }
+            Console.WriteLine($"The word {word} is a palindrome");
+        }   
     }
 }
